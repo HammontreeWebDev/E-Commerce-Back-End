1b23ea1c02d14d43a24cbb8c193f08ca74c5c48a
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-11-08 21:02:41 -0500

    forgot to add req.body to put req

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,14 +44,26 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  const categoryData = await Category.update(
-    {
-      where: {
-        id: req.params.id,
-      },
-    });
-  res.json(categoryData);
+  try {
+    const categoryData = await Category.update(req.body,
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
